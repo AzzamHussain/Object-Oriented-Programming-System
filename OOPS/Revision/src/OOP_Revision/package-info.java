e4da6816f3e581ac73c57325020a7b71ad3d9774
Azzam Hussain <126112158+AzzamHussain@users.noreply.github.com>
2024-01-02 13:14:13 -0800

    Add files via upload

---
+++ OOPS/Revision/src/OOP_Revision/package-info.java
@@ -0,0 +1,11 @@
+package OOP_Revision;
+class Animal{
+	void bark() {
+		System.out.println("Bhaooww Bhaoow");
+	}
+	public static void main(String[]args) {
+		System.out.println("3");
+		Animal retriever=new Animal();
+		retriever.bark();
+	}
+}
