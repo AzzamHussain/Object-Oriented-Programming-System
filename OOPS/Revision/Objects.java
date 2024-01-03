18ecb0fa667aee4efb7ab5f2f45dc9434136cc69
Azzam Hussain <126112158+AzzamHussain@users.noreply.github.com>
2024-01-03 12:33:16 -0800

    Objects Initialization

---
+++ OOPS/Revision/Objects.java
@@ -0,0 +1,18 @@
+
+//How To Initialize A Object By Reference Variable//
+//class Animal{
+
+public class Objects {
+        String color;
+        int age;
+        public static void main(String[]args){
+            Objects car=new Objects();
+            car.color="Black";
+            car.age=4;
+            System.out.println(car.color+" "+car.age);
+
+        }
+    }
+
+    
+//}
