2f10607ab4388411271266cd5c131199b0bf17d1
AzzamHussain <azzamhussain70@gmail.com>
2024-01-10 02:10:00 +0500

    MethodOverloading

---
+++ .vscode/Test.java
@@ -0,0 +1,12 @@
+ class Teston{
+    void ABC(int a){
+        System.out.println("90");
+    }
+     void ABC(String a){
+        System.out.println("*******");
+    }
+    public static void main(String[] args) {
+        Teston t=new Teston();
+        t.ABC("Azzam");
+    }
+}
