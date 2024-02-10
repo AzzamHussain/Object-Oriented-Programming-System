eaefbbc67937f10131f9d045ddf2fcd8439912e9
Azzam Hussain <126112158+AzzamHussain@users.noreply.github.com>
2024-02-11 01:44:58 +0500

    Final_Variable

---
+++ FinalKeyword.java
@@ -0,0 +1,7 @@
+public class FinalKeyword {
+    public static void main(String[] args) {
+         final int i=89;
+        //i=i+3;//This line gives error because if we initialize the final variable then its value cannot be changed
+        System.out.println(i);
+    }
+}
